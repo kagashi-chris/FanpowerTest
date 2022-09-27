e207c5dfdb1f75094c41b0bba8975fe7c44b3e99
Chris Zhen <chris.zhenartist@gmail.com>
2022-09-27 14:40:38 -0400

    changed from public to build

--- server/app.js
+++ server/app.js
@@ -12,10 +12,10 @@ app.use(express.json());
 app.use("/api", require("./api"));
 
 app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "..", "../public/index.html"))
+  res.sendFile(path.join(__dirname, "..", "../build/index.html"))
 );
 
-app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(express.static(path.join(__dirname, "..", "build")));
 
 app.use((req, res, next) => {
   if (path.extname(req.path).length) {
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 });
 
 app.use("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "client/public/index.html"));
+  res.sendFile(path.join(__dirname, "..", "client/build/index.html"));
 });
 
 app.use((err, req, res, next) => {
