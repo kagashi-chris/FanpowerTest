1682dac00e7a5cf8d06e57183fb0a6b164d8780e
Chris Zhen <chris.zhenartist@gmail.com>
2022-09-27 01:16:39 -0400

    updated server js

--- server/index.js
+++ server/index.js
@@ -1,19 +1,15 @@
-const path = require("path");
 const express = require("express");
+const app = express();
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 
-const app = express();
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "..", "client/public")));
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client/public/index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Sever listening on ${PORT}`);
+  console.log(`Server listening on ${PORT}`);
 });
