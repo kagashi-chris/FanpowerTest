d49f415556dd48f07c06e2bcc8a2dd4df33dd691
Chris Zhen <chris.zhenartist@gmail.com>
2022-09-27 01:27:32 -0400

    updated server js

--- server/index.js
+++ server/index.js
@@ -5,9 +5,9 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    req.sendFile(path.resolve(__dirname, "build", "index.html"));
+    req.sendFile(path.resolve(__dirname, "client/build", "index.html"));
   });
 }
 app.listen(PORT, () => {
