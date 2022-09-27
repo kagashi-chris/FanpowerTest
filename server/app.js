61237ac9e9a70aa87cbb60bb6a05bba024ca6e15
Chris Zhen <chris.zhenartist@gmail.com>
2022-09-27 15:14:27 -0400

    fix heroku database bug

--- server/app.js
+++ server/app.js
@@ -1,38 +1,40 @@
-// const path = require("path");
-// const express = require("express");
-// const morgan = require("morgan");
-// const app = express();
-// module.exports = app;
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
 
-// // logging middleware
-// app.use(morgan("dev"));
-// // body parsing middleware
-// app.use(express.json());
+module.exports = app;
 
-// app.use("/api", require("./api"));
+// logging middleware
+app.use(morgan("dev"));
+// body parsing middleware
+app.use(express.json());
 
-// app.get("/", (req, res) =>
-//   res.sendFile(path.join(__dirname, "..", "../build/index.html"))
-// );
+// auth and api routes
+// app.use('/auth', require('./auth'))
+app.use("/api", require("./api"));
 
-// app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
-// app.use((req, res, next) => {
-//   if (path.extname(req.path).length) {
-//     const err = new Error("Not found");
-//     err.status = 404;
-//     next(err);
-//   } else {
-//     next();
-//   }
-// });
+// any remaining requests with an extension (.js, .css, etc.) send 404
+app.use((req, res, next) => {
+  if (path.extname(req.path).length) {
+    const err = new Error("Not found");
+    err.status = 404;
+    next(err);
+  } else {
+    next();
+  }
+});
 
-// app.use("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "..", "client/build/index.html"));
-// });
+// sends index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
 
-// app.use((err, req, res, next) => {
-//   console.error(err);
-//   console.error(err.stack);
-//   res.status(err.status || 500).send(err.message || "Internal server error.");
-// });
+// error handling endware
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || "Internal server error.");
+});
--- server/index.js
+++ server/index.js
@@ -1,16 +1,56 @@
+// const path = require("path");
+// const express = require("express");
+
+// const PORT = process.env.PORT || 3001;
+
+// const app = express();
+
+// app.use("/api", require("./api"));
+
+// app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server listening on ${PORT}`);
+// });
+
 const path = require("path");
 const express = require("express");
-
-const PORT = process.env.PORT || 3001;
-
 const app = express();
+const PORT = process.env.PORT || 3001;
+module.exports = app;
+
+// body parsing middleware
+app.use(express.json());
 
 app.use("/api", require("./api"));
 
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
+// any remaining requests with an extension (.js, .css, etc.) send 404
+app.use((req, res, next) => {
+  if (path.extname(req.path).length) {
+    const err = new Error("Not found");
+    err.status = 404;
+    next(err);
+  } else {
+    next();
+  }
+});
+
+// sends index.html
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
+
+// error handling endware
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || "Internal server error.");
 });
 
 app.listen(PORT, () => {
